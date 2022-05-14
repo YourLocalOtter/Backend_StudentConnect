783bec08705f22e1227cad2747e9044e8c677eb0
Raymomyar <awesomecatstudio@gmail.com>
2022-05-14 16:00:35 -0700

    validate emails

--- main.py
+++ main.py
@@ -46,6 +46,8 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 	db_user = crud.get_user_by_email(db, email=user.email)
 	if db_user:
 		raise HTTPException(status_code=400, detail="Email already registered")
+	if not user.email or not "@" in user.email: # check if it is actually a email
+		raise HTTPException(status_code=400, detail="Invalid email")
 	return crud.create_user(db=db, user=user)
 
 
